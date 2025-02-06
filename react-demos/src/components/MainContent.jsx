630a8d6f02caee61d861db503e72b69b55fbb67b
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-02-06 20:25:28 +0200

    feature: added MainContent component initial code

---
+++ react-demos/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MainContent() {
+  return (
+    <main>
+        <h2>Main Content</h2>
+        <p>God Thank you for answering my prayers.</p>
+    </main>
+  )
+}
