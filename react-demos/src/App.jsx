fba5ae008e486da144141fd27090b8e41aac46ca
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-02-06 21:03:13 +0200

    feature: added WelcomeMessage component initial code

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,7 +1,15 @@
-import Greet from "./Greet";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 export default function App() {
   return (
-    <Greet/>
+    <div>
+      <WelcomeMessage/>
+      <Header/>
+      <MainContent/>
+      <Footer/>
+    </div>
   )
 }
---
+++ react-demos/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function WelcomeMessage() {
+  return (
+    <div className="">
+        <h1>Hello, World!</h1>
+        <p>Welcome to learning JSX!</p>
+    </div>
+  )
+}
