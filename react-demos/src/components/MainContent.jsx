7cdf768d2390a1ec47637bd246af81626c48aaf2
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-02-06 22:20:24 +0200

    feature: added expressions and working with lists

--- react-demos/src/components/MainContent.jsx
+++ react-demos/src/components/MainContent.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 
 export default function MainContent() {
+  const usersInfo = [
+    {
+      "username": "TechGuru88",
+      "email": "techguru88@example.com",
+      "location": "San Francisco, CA"
+    },
+    {
+      "username": "DigitalNomad2025",
+      "email": "nomad.online@domain.net",
+      "location": "Bali, Indonesia"
+    },
+    {
+      "username": "CodeQueen",
+      "email": "codequeen@sample.org",
+      "location": "London, UK"
+    },
+    {
+      "username": "AdventureDude",
+      "email": "adventure.guy@provider.co",
+      "location": "Banff, Canada"
+    },
+    {
+      "username": "SunriseLover",
+      "email": "sun.shine@email.info",
+      "location": "Sydney, Australia"
+    }
+  ]
+
   return (
     <main>
-        <h2>Main Content</h2>
-        <p>God Thank you for answering my prayers.</p>
+      {usersInfo.map((user, i) => (
+        <ul key={i}>
+          <li>{user.username}</li>
+          <li>{user.email}</li>
+          <li>{user.location}</li>
+        </ul>
+      ))}
     </main>
   )
 }
--- react-demos/src/components/WelcomeMessage.jsx
+++ react-demos/src/components/WelcomeMessage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 export default function WelcomeMessage() {
+  const name = "Albert"
+  const today = new Date()
+
   return (
     <div className="">
-        <h1>Hello, World!</h1>
-        <p>Welcome to learning JSX!</p>
+        <h1>Hello, {name}!</h1>
+        <p>Welcome to learning JSX! {today.getDate()}</p>
     </div>
   )
 }
